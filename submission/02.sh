7b96e759ee84d4172b84dbdc656d41645e53e18a
elnafateh <elnafatehh@gmail.com>
2026-07-29 12:22:40 +0100

    feat: 01-04

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # How many new outputs were created by block 243,825?
+HASH=$(bitcoin-cli -signet getblockhash 243825)
+bitcoin-cli -signet getblock "$HASH" 2 | jq '[.tx[].vout] | flatten | length'
--- submission/03.sh
+++ submission/03.sh
@@ -1,1 +1,6 @@
 # Which tx in block 216,351 spends the coinbase output of block 216,128?
+CB_HASH=$(bitcoin-cli -signet getblockhash 216128)
+COINBASE_TXID=$(bitcoin-cli -signet getblock "$CB_HASH" 2 | jq -r '.tx[0].txid')
+
+TARGET_HASH=$(bitcoin-cli -signet getblockhash 216351)
+bitcoin-cli -signet getblock "$TARGET_HASH" 2 | jq -r --arg txid "$COINBASE_TXID" '.tx[] | select(.vin[].txid == $txid) | .txid'
